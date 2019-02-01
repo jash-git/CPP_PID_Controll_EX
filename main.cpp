d312d985c2fa7ee463cc325012dadb66c80ca3fb
jash.liao <jash.liao@gmail.com>
2019-02-01 21:57:45 +0800

    modified 2019/02/01

--- main.cpp
+++ main.cpp
@@ -11,9 +11,18 @@
 using namespace std;
 //https://gist.github.com/bradley219/5373998
 //PID 控制器實作 數字追逐賽
+//https://zh.wikipedia.org/wiki/%E9%BD%90%E6%A0%BC%E5%8B%92%EF%BC%8D%E5%B0%BC%E7%A7%91%E5%B0%94%E6%96%AF%E6%96%B9%E6%B3%95
 int main()
 {
-    PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);//0.1s=10Hz,outmax=100,outmin=-100,P=0.1,I=0.01,D=0.5
+    //PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);//0.1s=10Hz,outmax=100,outmin=-100,P=0.1,D=0.01,I=0.5
+
+    //Ziegler–Nichols方法
+    //Ku=2,Tu=0.2
+    //P=2*0.2
+    //D=2*0.2*0.2/3
+    //I=2*(2*0.2)/0.2
+    PID pid = PID(0.1, 100, -100, (0.2*2), ((2*0.2)*0.2/3), (2*(2*0.2)/0.2));
+
     double val = 10.0;
     double setpoint = 50.0;
     double inc=0.0;
@@ -26,7 +35,7 @@ int main()
     {
         Sleep( 100 );//100ms=0.1s
         inc = pid.calculate(setpoint, val);//setpoint(目標[期望]),input(輸入)
-        //printf("i=%d,val:% 7.5f inc:% 7.5f\n",i, val, inc);//顯示小數點五位
+        printf("i=%d,val:% 7.5f inc:% 7.5f\n",i, val, inc);//顯示小數點五位
         if(fabs(val-setpoint)<0.0001)//小數點四位
         {
             break;
@@ -35,7 +44,7 @@ int main()
         i++;
     }
 
-    printf("i=%d,val:% 7.5f inc:% 7.5f\n",i, val, inc);//顯示小數點五位
+    //printf("i=%d,val:% 7.5f inc:% 7.5f\n",i, val, inc);//顯示小數點五位
 
 	time_t t02 = time(NULL);
 	struct tm tm02 = *localtime(&t02);
